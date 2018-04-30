6615adbcdf2efba32e09ba2e96c2f0a527ae852d
marifolsom <mfolsom1@fordham.edu>
2018-04-30 18:12:20 -0400

    cleaned up nav bar code

--- Palettable/src/screens/MainMenu/index.js
+++ Palettable/src/screens/MainMenu/index.js
@@ -4,41 +4,17 @@ import { Text, View, Button } from 'react-native';
 import startMainTabs from '../MainTabs/startMainTabs';
 
 class MainMenuScreen extends Component {
-  navHandler(tabsArray) {
-    const tabs = { tabs: tabsArray }
-    startMainTabs(tabs);
+  navHandler() {
+    startMainTabs();
   }
 
   render() {
-    const tabsArray = [
-      {
-        screen: 'palettable.DiscoverScreen',
-        label: 'Discover',
-        title: 'Discover'
-      },
-      {
-        screen: 'palettable.GenerateScreen',
-        label: 'Generate',
-        title: 'Generate'
-      },
-      {
-        screen: 'palettable.FavoritesScreen',
-        label: 'Favorites',
-        title: 'Favorites'
-      },
-      {
-        screen: 'palettable.AuthScreen',
-        label: 'Login',
-        title: 'Login'
-      }
-    ];
-
     return (
       <View>
-        <Button title="Discover" onPress={() => this.navHandler(tabsArray)} />
-        <Button title="Generate" onPress={() => this.navHandler(tabsArray)} />
-        <Button title="Favorites" onPress={() => this.navHandler(tabsArray)} />
-        <Button title="Login" onPress={() => this.navHandler(tabsArray)} />
+        <Button title="Discover" onPress={() => this.navHandler()} />
+        <Button title="Generate" onPress={() => this.navHandler()} />
+        <Button title="Favorites" onPress={() => this.navHandler()} />
+        <Button title="Login" onPress={() => this.navHandler()} />
       </View>
     )
   }
--- Palettable/src/screens/MainTabs/startMainTabs.js
+++ Palettable/src/screens/MainTabs/startMainTabs.js
@@ -1,30 +1,30 @@
 import { Navigation } from 'react-native-navigation';
 
-
-const startTabs = (tabs) => {
-  Navigation.startTabBasedApp(tabs)
+const startTabs = () => {
+  Navigation.startTabBasedApp({
+    tabs: [
+      {
+        screen: 'palettable.DiscoverScreen',
+        label: 'Discover',
+        title: 'Discover'
+      },
+      {
+        screen: 'palettable.GenerateScreen',
+        label: 'Generate',
+        title: 'Generate'
+      },
+      {
+        screen: 'palettable.FavoritesScreen',
+        label: 'Favorites',
+        title: 'Favorites'
+      },
+      {
+        screen: 'palettable.AuthScreen',
+        label: 'Login',
+        title: 'Login'
+      }
+    ]
+  })
 }
-
-// const startTabs = () => {
-//   Navigation.startTabBasedApp({
-//     tabs: [
-//       {
-//         screen: 'palettable.DiscoverScreen',
-//         label: 'Discover',
-//         title: 'Discover'
-//       },
-//       {
-//         screen: 'palettable.GenerateScreen',
-//         label: 'Generate',
-//         title: 'Generate'
-//       },
-//       {
-//         screen: 'palettable.AuthScreen',
-//         label: 'Login',
-//         title: 'Login'
-//       }
-//     ]
-//   })
-// }
 
 export default startTabs;
