8e1ae29b8ef193f7a05b9d78108f08e76a8381af
marifolsom <mfolsom1@fordham.edu>
2018-05-01 10:27:15 -0400

    add react-native-image-picker

--- Palettable/android/app/src/main/java/com/palettable/MainApplication.java
+++ Palettable/android/app/src/main/java/com/palettable/MainApplication.java
@@ -3,6 +3,8 @@ package com.palettable;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativenavigation.NavigationReactPackage;
+import com.imagepicker.ImagePickerPackage;
 import org.reactnative.camera.RNCameraPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new NavigationReactPackage(),
+            new ImagePickerPackage(),
             new RNCameraPackage()
       );
     }
