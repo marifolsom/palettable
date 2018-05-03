0b74cc0a51f74e74105415e064dd58c5b9a19289
marifolsom <mfolsom1@fordham.edu>
2018-05-03 17:50:53 -0400

    camera now works!

--- Palettable/src/screens/Discover/index.js
+++ Palettable/src/screens/Discover/index.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 import Palette from '../../components/Palette';
 
 class DiscoverScreen extends Component {
   render() {
     return (
-      <View>
+      <View >
         <Palette />
       </View>
     )
--- Palettable/src/screens/Generate/index.js
+++ Palettable/src/screens/Generate/index.js
@@ -24,8 +24,8 @@ class GenerateScreen extends Component {
       const data = await this.camera.capture();
       // Image source needs an object with a uri property
       // Still playing around with the URI and getting the taken photo to show
-      pathHTTPS = `https://${data.path}`
-      const path = { uri: pathHTTPS, path: data.path }
+      pathHTTPS = `https://${data.path}`;
+      const path = { uri: pathHTTPS, path: data.path };
       this.setState({
         photoInfo: path
       })
@@ -53,7 +53,9 @@ class GenerateScreen extends Component {
           rgbaArray = swatch.color.split(/[(), ]/);
           // console.log(rgbaArray); // looks like: ["rgba", "121", "", "121", "", "138", "", "1", ""]
           const hexValue = rgbToHex(Number(rgbaArray[1]), Number(rgbaArray[3]), Number(rgbaArray[5]));
-          hexValueArray.push(hexValue);
+          if (hexValueArray.length < 5) {
+            hexValueArray.push(hexValue);
+          }
         })
         this.setState({
           palette: hexValueArray
@@ -77,7 +79,7 @@ class GenerateScreen extends Component {
                   ref={cam => (this.camera = cam)}
                   aspect={Camera.constants.Aspect.fill}
                   type={Camera.constants.Type.back}
-                  style={styles.preview}
+                  style={styles.container}
                   flashMode={Camera.constants.FlashMode.off}
                   permissionDialogTitle={"Permission to use camera"}
                   permissionDialogMessage={"We need permission to use the camera on your phone"}
@@ -88,10 +90,10 @@ class GenerateScreen extends Component {
               </View>
             ) : (
               // Otherwise, display the taken image along with a 'Generate!' button
-              <View style={styles.imageContainer}>
-                <Image source={this.state.photoInfo} style={styles.imageContainer} />
+              <View style={styles.container}>
+                <Image source={this.state.photoInfo} style={styles.container} />
                 <View style={styles.buttons}>
-                  {/* <Button title="Retake" onPress={this.imagePickerHandler} /> */}
+                  <Button title="Retake" onPress={this.imagePickerHandler} />
                   <Button title="Generate!" onPress={this.generatePaletteHandler} />
                 </View>
               </View>
@@ -107,25 +109,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
-  },
-  imageContainer: {
-    justifyContent: 'center',
     alignItems: 'center',
-    height: 500,
     width: 375
   },
   buttons: {
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between'
-  },
-  preview: {
-    flex: 1,
-    justifyContent: "flex-end",
-    alignItems: "center",
-    height: 500,
-    width: 375
   }
 })
 
